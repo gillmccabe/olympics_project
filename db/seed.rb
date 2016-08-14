35a92cf5068b3103913f0a27ab3ebf9c16948ced
Gillian McCabe <gillianmccabe@outlook.com>
2016-08-14 21:00:26 +0100

    seed file updated

--- db/seed.rb
+++ db/seed.rb
@@ -29,7 +29,6 @@ athlete6 = Athlete.new({ 'name' => 'Ruth Beitia', 'nation_id' => nation3.id})
 athlete7 = Athlete.new({ 'name' => 'Michael Phelps', 'nation_id' => nation4.id})
 athlete8 = Athlete.new({ 'name' => 'Serena Williams', 'nation_id' => nation4.id})
 
-
 athlete1.save
 athlete2.save
 athlete3.save
@@ -41,18 +40,43 @@ athlete8.save
 
 
 event1 = Event.new({ 'name' => 'Tennis', 'gold_id' => athlete1.id, 'silver_id' => athlete2.id, 'bronze_id' => athlete3.id})
+event2 = Event.new({ 'name' => 'Swimming', 'gold_id' => athlete4.id, 'silver_id' => athlete2.id, 'bronze_id' => athlete7.id})
+event3 = Event.new({ 'name' => 'Athletics', 'gold_id' => athlete5.id, 'silver_id' => athlete1.id, 'bronze_id' => athlete8.id})
 
 event1.save
+event2.save
+event3.save
 
 
 participation1 = Participation.new({'athlete_id' => athlete1.id, 'event_id' => event1.id})
 participation2 = Participation.new({'athlete_id' => athlete2.id, 'event_id' => event1.id})
 participation3 = Participation.new({'athlete_id' => athlete3.id, 'event_id' => event1.id})
+participation4 = Participation.new({'athlete_id' => athlete7.id, 'event_id' => event2.id})
+participation5 = Participation.new({'athlete_id' => athlete5.id, 'event_id' => event2.id})
+participation6 = Participation.new({'athlete_id' => athlete3.id, 'event_id' => event2.id})
+participation7 = Participation.new({'athlete_id' => athlete6.id, 'event_id' => event2.id})
+participation8 = Participation.new({'athlete_id' => athlete1.id, 'event_id' => event2.id})
+participation9 = Participation.new({'athlete_id' => athlete2.id, 'event_id' => event3.id})
+participation10 = Participation.new({'athlete_id' => athlete8.id, 'event_id' => event3.id})
+participation11 = Participation.new({'athlete_id' => athlete3.id, 'event_id' => event3.id})
+participation12 = Participation.new({'athlete_id' => athlete7.id, 'event_id' => event3.id})
+participation13 = Participation.new({'athlete_id' => athlete1.id, 'event_id' => event3.id})
+participation14 = Participation.new({'athlete_id' => athlete5.id, 'event_id' => event3.id})
 
 participation1.save
 participation2.save
 participation3.save
-
+participation4.save
+participation5.save
+participation6.save
+participation7.save
+participation8.save
+participation9.save
+participation10.save
+participation11.save
+participation12.save
+participation13.save
+participation14.save
 
 
 
