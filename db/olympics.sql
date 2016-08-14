6c53b9ee787b1bc2f502abfbabb62c119177e14b
Gillian McCabe <gillianmccabe@outlook.com>
2016-08-14 15:07:06 +0100

    Database added

---
+++ db/olympics.sql
@@ -0,0 +1,30 @@
+DROP TABLE participation;
+DROP TABLE events;
+DROP TABLE athletes;
+DROP TABLE nations;
+
+
+CREATE TABLE nations(
+  id serial4 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE atheletes(
+  id serial4 PRIMARY KEY,
+  name VARCHAR(255)
+  nation_id INT4 references nations(id) ON DELETE CASCADE
+);
+
+CREATE TABLE events(
+  id serial4 PRIMARY KEY,
+  name VARCHAR(255),
+  gold_id references athletes(id) ON DELETE CASCADE,
+  silver_id INT4 references athletes(id) ON DELETE CASCADE,
+  bronze_id INT4 references athletes(id) ON DELETE CASCADE
+);
+
+CREATE TABLE participation(
+  id serial4 PRIMARY KEY,
+  athlete_id INT4 references athletes(id) ON DELETE CASCADE,
+  event_id INT4 references events(id) ON DELETE CASCADE
+);
